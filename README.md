b6a7e4b71155631b928aa4636de541946bacf0d0
Gregory Jarrige <gregory.jrrg@gmail.com>
2019-01-23 23:13:02 +0100

    ajout sketch17 + patch PJS

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # pjs3D_experiments
 Objectif : tester la portabilité de code Processing vers ProcessingJS (en particulier la 3D et le support de WebGL)
 
-Le but de ce projet personnel et assez expérimental est de tester la bonne portabilité du code Processing vers ProcessingJS.
-Priorité a été donnée au test de sketchs utilisant un rendu 3D vers WebGL. Les résultats obtenus sont très encourageants, et devraient faire plaisir à tous ceux qui ont développé des projets 3D avec Processing, et se demandent comment porter ces projets sur le web (sachant que le projet concurrent de ProcessingJS, n'atteint pas un aussi bon niveau de portabilité pour la 3D, à mon grand regret d'ailleurs).
+Le but de ce projet est de tester la portabilité de sketch Processing vers ProcessingJS.
+
+Priorité a été donnée au test de sketchs utilisant un rendu 3D vers WebGL. Les résultats obtenus sont très encourageants, et devraient faire plaisir à tous ceux qui ont développé des projets 3D avec Processing, et se demandent comment porter ces projets sur le web (une alternative étant le projet P5js).
 
 Pour effectuer ces tests, j'ai choisi arbitrairement d'utiliser ProcessingJS comme une "pure JS library", comme expliqué à la fin du "Pomax Guide". Ce choix a été dicté par le fait que je suis un développeur Javascript, et non Java, et que je souhaite utiliser ProcessingJS de cette manière. Mais vous pouvez utiliser ProcessingJS de plusieurs façons, comme expliqué dans le "Pomax Guide", à vous de trouver le mode qui vous convient le mieux.
 
-Pour les tests relatifs à la 3D, j'ai repris et adapté plusieurs sketchs tirés d'un très bon livre de Jan Vantomme sur Processing2 (cf. référence en bas de page). J'ai ajouté à certains de ces sketchs quelques fonctionnalités telles que zoom, et changement de rendu additionnel.
-
 ATTENTION : pour faire fonctionner le projet, téléchargez le localement et ouvrez la page index.html (qui se trouve dans le répertoire "project") avec votre navigateur. Cette page est le point d'entrée vers les différents exemples.
+
+Pour les tests relatifs à la 3D, j'ai repris et adapté plusieurs sketchs tirés de plusieurs très bons livres, dont les références sont indiquées en bas de la page d'accueil (index.html).
 
 Lien vers le "Pomax Guide" :
 http://processingjs.org/articles/PomaxGuide.html
--- project/sketch17.js
+++ project/sketch17.js
@@ -1,64 +1,206 @@
-
 (function(){
   "use strict";
 
   var canvas = document.getElementById('glibcanvas');
   var pjs = new Processing(canvas);
 
-  var texture1;
-  var texture2;
-
-// preload ne fonctionne pas dans ce mode d'exécution
-/* @pjs preload="assets/texture1.jpg, assets/texture2.jpg"; */
-
-// count , radius , twist, hcount, phase, hradius
-  var data_form = {};
-
-  var vertx = [];
-  var verty = [];
-
-  var pjs_form = document.querySelectorAll("[data-form]");
-  [].forEach.call(pjs_form, item => {
-      data_form[item.id] = item.value;
-      item.addEventListener('change', function(evt) {
-      let output = document.querySelector(`[data-name=${this.id}]`);
-      data_form[this.id] = this.value;
-      output.value = this.value;
-    }, false)
-  });
-
-  function getR( a, v1, v2 ) {
-    return pjs.sin( pjs.radians(a) * v1 ) * v2 + 100;
-  }
-  function initPoints(v1, v2) {
-    for( let a = 0; a < 72; a++) {
-      vertx[a] = pjs.cos( pjs.radians( a * 5.0 )) * getR( a * 5.0, v1, v2 );
-      verty[a] = pjs.sin( pjs.radians( a * 5.0 )) * getR( a * 5.0, v2, v2 );
+  class Quaternion {
+    constructor() {
+      this.w = 1.0;
+      this.x = 0.0;
+      this.y = 0.0;
+      this.z = 0.0;
+    }
+    mult( b ) {  // void mult( Quaternion b )
+      let res = new Quaternion();
+      res.w = this.w * b.w - this.x * b.x - this.y * b.y - this.z * b.z;
+      res.x = this.w * b.x + this.x * b.w + this.y * b.z - this.z * b.y;
+      res.y = this.w * b.y - this.x * b.z + this.y * b.w + this.z * b.x;
+      res.z = this.w * b.z + this.x * b.y - this.y * b.x + this.z * b.w;
+      this.setQuaternion( res );
+    }
+    setCrossDot( cross, dot ) { // void set( PVector cross, float dot )
+      this.x = cross.x;
+      this.y = cross.y;
+      this.z = cross.z;
+      this.w = dot;
+    }
+    setQuaternion( ref ) { // void set( Quaternion ref )
+      this.w = ref.w;
+      this.x = ref.x;
+      this.y = ref.y;
+      this.z = ref.z;
+    }
+    matrix() { // float[] matrix()
+      let res = [];
+      let sa = Math.sqrt(1.0 - this.w * this.w);
+      if (sa < pjs.EPSILON) {
+        sa = 1.0;
+      }
+      res[0] = Math.acos(this.w) * 2.0;
+      res[1] = this.x / sa;
+      res[2] = this.y / sa;
+      res[3] = this.z / sa;
+      return res;
     }
   }
 
+  var texture1;
+  var texture2;
+
+  var down = null;
+  var qdown = new Quaternion();
+  var qnow = new Quaternion();
+  var qdrag = new Quaternion();
+
+  var max_width = 800;
+  var max_height = 600;
+  var width_by_2 = max_width/2
+  var height_by_2 = max_height/2;
+  var max_tri_strips = 30;
+  var max_vertices = 72;
+  var vertx = [];
+  var verty = [];
+
+  /*
+  <label>Count : <output data-name="pjs_count" for="pjs_count"></output>
+    <input type="range" min="1" max="100" value="50" class="slider" id="pjs_count">
+  </label>
+  */
+  function addSlider( fieldname, domtarget, start, end, value, label ) {
+    let xlabel = document.createElement('label');
+    xlabel.innerHTML = label + ' : ';
+    let xoutput = document.createElement('output');
+    xoutput.setAttribute('for', 'pjs_'+fieldname);
+    xoutput.value = value;
+    xlabel.append(xoutput);
+    let xinput = document.createElement('input');
+    xinput.setAttribute('type', 'range');
+    xinput.setAttribute('min', start);
+    xinput.setAttribute('max', end);
+    xinput.setAttribute('class', 'slider');
+    xinput.setAttribute('id', 'pjs_'+fieldname);
+    xinput.value = value;
+    xlabel.append(xinput);
+    domtarget.append(xlabel);
+    xinput.addEventListener('change', function(evt) {
+      let output = evt.target.parentNode.querySelector('output');
+      output.value = this.value;
+      data_form[this.id] = this.value;
+    }, false)
+
+  }
+
+  var filters = [];   // count , radius , twist, hcount, phase, hradius
+  filters.push({field:"count", min:1, max:20, value:5, label:"Count"});
+  filters.push({field:"radius", min:-10, max:10, value:5, label:"Radius"});
+  filters.push({field:"twist", min:-2, max:2, value:2, label:"Twist"});
+  filters.push({field:"hcount", min:0, max:2, value:1.5, label:"HCount"});
+  filters.push({field:"phase", min:0, max:4, value:2, label:"Phase"});
+  filters.push({field:"hradius", min:0, max:5, value:5, label:"HRadius"});
+
+  var data_form = {};
+
+  var form = document.getElementById('form');
+  filters.forEach(item => {
+    addSlider(item.field, form, item.min, item.max, item.value, item.label);
+    data_form['pjs_'+item.field] = item.value;
+  })
+
+  function getR( a, h) {
+    //return pjs.sin( pjs.radians(a) * v1 ) * v2 + 100;
+    return  data_form.pjs_radius * pjs.sin( pjs.radians(a)
+            * data_form.pjs_count
+            + ( h / 15 )* data_form.pjs_twist)
+            + pjs.sin(pjs.radians(3.6 * h) * data_form.pjs_hcount + data_form.pjs_phase)
+            * data_form.pjs_hradius + 40;
+  }
+
+  function initPoints() {
+    for ( let h = 0; h < max_tri_strips; h++) {
+      vertx[h] = [];
+      verty[h] = [];
+      for ( let a = 0; a < max_vertices; a++) {
+        let r = getR( a * 5.0, h * 5.0 );
+        vertx[h][a] = pjs.cos( pjs.radians( a*5.0 )) * r;
+        verty[h][a] = pjs.sin( pjs.radians( a*5.0 )) * r;
+      }
+    }
+  }
+
+
   pjs.setup = function(){
-    pjs.size( 400, 400, pjs.OPENGL );
+    pjs.size( max_width, max_height, pjs.P3D );
     pjs.loop();
   }
 
   pjs.draw = function() {
+    pjs.background( 255 );
+    pjs.lights();
 
     pjs.pushMatrix();
-    pjs.background(255);
-    pjs.translate( pjs.width/2, pjs.height/2 );
-    pjs.noFill();
-    initPoints(data_form.pjs_count, data_form.pjs_radius);
-    pjs.beginShape();
-    for( let a = 0; a < 72; a++) {
-      pjs.vertex( vertx[a], verty[a] );
-    }
-    pjs.vertex( vertx[0], verty[0] );
-    pjs.endShape();
-    pjs.popMatrix();
+    pjs.translate( width_by_2, height_by_2);
+    pjs.scale( 1.5 );
 
+    qnow.setQuaternion( qdrag );
+    qnow.mult( qdown );
+    var matrix = qnow.matrix();
+    pjs.rotate( matrix[0], matrix[1], matrix[2], matrix[3] );
+
+    pjs.translate(0, -50, 0);
+    pjs.fill( 255, 0, 0 );
+
+    initPoints(data_form.pjs_count, data_form.pjs_radius);
+
+    pjs.beginShape(pjs.TRIANGLE_STRIP );
+    for ( let h = 1; h < max_tri_strips; h++) {
+      for ( let a = 0; a <= max_vertices; a++ ) {
+        let aa = a % max_vertices;
+        pjs.normal( vertx[h][aa], 0, verty[h][aa]);
+        pjs.vertex( vertx[h][aa], h*5.0, verty[h][aa] );
+        pjs.normal( vertx[h-1][aa], 0, verty[h-1][aa]);
+        pjs.vertex( vertx[h-1][aa], (h-1)*5.0, verty[h-1][aa] );
+      }
+    }
+    pjs.endShape();
+
+    pjs.beginShape(pjs.TRIANGLE_FAN);
+    let h = max_tri_strips - 1;
+    pjs.vertex( 0, h*5, 0 );
+    for ( let a = 0; a <= max_vertices; a++ ) {
+      let aa = a % max_vertices;
+      pjs.vertex( vertx[h][aa], h*5, verty[h][aa] );
+    }
+    pjs.endShape();
+
+    pjs.popMatrix();
   }
 
-   // kickstart the sketch
-   pjs.setup();
+
+  function mouseToSphere(x, y) {
+    let v = new pjs.PVector();
+    v.x = (x - width_by_2) / data_form.pjs_radius;
+    v.y = (y - height_by_2) / data_form.pjs_radius;
+    let mag = v.x * v.x + v.y * v.y;
+    if (mag > 1.0) {
+      v.normalize();
+    } else {
+      v.z = pjs.sqrt(1.0 - mag);
+    }
+    return v;
+  }
+
+  pjs.mousePressed = function() {
+      down = mouseToSphere( pjs.mouseX, pjs.mouseY );
+      qdown.setQuaternion( qnow );
+      qdrag = new Quaternion();
+  }
+
+  pjs.mouseDragged = function() {
+      let drag = mouseToSphere( pjs.mouseX, pjs.mouseY );
+      qdrag.setCrossDot( down.cross( drag), down.dot( drag ));
+  }
+
+  // kickstart the sketch
+  pjs.setup();
 })();
